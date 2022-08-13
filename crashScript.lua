2342416aac44ead658156bd097d8980424801659
robloxianstudio123 <63188671+robloxianstudio123@users.noreply.github.com>
2022-08-13 13:06:33 +0200

    low budget crash
    yes

---
+++ crashScript.lua
@@ -0,0 +1,273 @@
+local SpawnAmount =800 -- the more, the better
+function audacity(par,nam)
+	while true do
+		for i=1,SpawnAmount do
+			local a=Instance.new("Tool",par)
+			a.Name=nam
+			local a=Instance.new("Folder",par)
+			a.Name=nam
+			local a=Instance.new("Frame",par)
+			a.Name=nam
+			local a=Instance.new("Configuration",par)
+			a.Name=nam
+			local a=Instance.new("Script",par)
+			a.Name=nam
+			local a=Instance.new("Model",par)
+			a.Name=nam
+			local a=Instance.new("Decal",par)
+			a.Name=nam
+			local a=Instance.new("DoubleConstrainedValue",par)
+			a.Name=nam
+			local a=Instance.new("DepthOfFieldEffect",par)
+			a.Name=nam
+			local a=Instance.new("Camera",par)
+			a.Name=nam
+			local a=Instance.new("Part",par)
+			a.Name=nam
+			local a=Instance.new("MeshPart",par)
+			a.Name=nam
+			local a=Instance.new("ParticleEmitter",par)
+			a.Name=nam
+			local a=Instance.new("Tool",par)
+			a.Name=nam
+			local a=Instance.new("Folder",par)
+			a.Name=nam
+			local a=Instance.new("Frame",par)
+			a.Name=nam
+			local a=Instance.new("Configuration",par)
+			a.Name=nam
+			local a=Instance.new("Script",par)
+			a.Name=nam
+			local a=Instance.new("Model",par)
+			a.Name=nam
+			local a=Instance.new("Decal",par)
+			a.Name=nam
+			local a=Instance.new("DoubleConstrainedValue",par)
+			a.Name=nam
+			local a=Instance.new("DepthOfFieldEffect",par)
+			a.Name=nam
+			local a=Instance.new("Camera",par)
+			a.Name=nam
+			local a=Instance.new("Part",par)
+			a.Name=nam
+			local a=Instance.new("MeshPart",par)
+			a.Name=nam
+			local a=Instance.new("ParticleEmitter",par)
+			a.Name=nam
+			local a=Instance.new("Tool",par)
+			a.Name=nam
+			local a=Instance.new("Folder",par)
+			a.Name=nam
+			local a=Instance.new("Frame",par)
+			a.Name=nam
+			local a=Instance.new("Configuration",par)
+			a.Name=nam
+			local a=Instance.new("Script",par)
+			a.Name=nam
+			local a=Instance.new("Model",par)
+			a.Name=nam
+			local a=Instance.new("Decal",par)
+			a.Name=nam
+			local a=Instance.new("DoubleConstrainedValue",par)
+			a.Name=nam
+			local a=Instance.new("DepthOfFieldEffect",par)
+			a.Name=nam
+			local a=Instance.new("Camera",par)
+			a.Name=nam
+			local a=Instance.new("Part",par)
+			a.Name=nam
+			local a=Instance.new("MeshPart",par)
+			a.Name=nam
+			local a=Instance.new("ParticleEmitter",par)
+			a.Name=nam
+			local a=Instance.new("Tool",par)
+			a.Name=nam
+			local a=Instance.new("Folder",par)
+			a.Name=nam
+			local a=Instance.new("Frame",par)
+			a.Name=nam
+			local a=Instance.new("Configuration",par)
+			a.Name=nam
+			local a=Instance.new("Script",par)
+			a.Name=nam
+			local a=Instance.new("Model",par)
+			a.Name=nam
+			local a=Instance.new("Decal",par)
+			a.Name=nam
+			local a=Instance.new("DoubleConstrainedValue",par)
+			a.Name=nam
+			local a=Instance.new("DepthOfFieldEffect",par)
+			a.Name=nam
+			local a=Instance.new("Camera",par)
+			a.Name=nam
+			local a=Instance.new("Part",par)
+			a.Name=nam
+			local a=Instance.new("MeshPart",par)
+			a.Name=nam
+			local a=Instance.new("ParticleEmitter",par)
+			a.Name=nam
+			local a=Instance.new("Tool",par)
+			a.Name=nam
+			local a=Instance.new("Folder",par)
+			a.Name=nam
+			local a=Instance.new("Frame",par)
+			a.Name=nam
+			local a=Instance.new("Configuration",par)
+			a.Name=nam
+			local a=Instance.new("Script",par)
+			a.Name=nam
+			local a=Instance.new("Model",par)
+			a.Name=nam
+			local a=Instance.new("Decal",par)
+			a.Name=nam
+			local a=Instance.new("DoubleConstrainedValue",par)
+			a.Name=nam
+			local a=Instance.new("DepthOfFieldEffect",par)
+			a.Name=nam
+			local a=Instance.new("Camera",par)
+			a.Name=nam
+			local a=Instance.new("Part",par)
+			a.Name=nam
+			local a=Instance.new("MeshPart",par)
+			a.Name=nam
+			local a=Instance.new("ParticleEmitter",par)
+			a.Name=nam
+			local a=Instance.new("Tool",par)
+			a.Name=nam
+			local a=Instance.new("Folder",par)
+			a.Name=nam
+			local a=Instance.new("Frame",par)
+			a.Name=nam
+			local a=Instance.new("Configuration",par)
+			a.Name=nam
+			local a=Instance.new("Script",par)
+			a.Name=nam
+			local a=Instance.new("Model",par)
+			a.Name=nam
+			local a=Instance.new("Decal",par)
+			a.Name=nam
+			local a=Instance.new("DoubleConstrainedValue",par)
+			a.Name=nam
+			local a=Instance.new("DepthOfFieldEffect",par)
+			a.Name=nam
+			local a=Instance.new("Camera",par)
+			a.Name=nam
+			local a=Instance.new("Part",par)
+			a.Name=nam
+			local a=Instance.new("MeshPart",par)
+			a.Name=nam
+			local a=Instance.new("ParticleEmitter",par)
+			a.Name=nam
+			local a=Instance.new("Tool",par)
+			a.Name=nam
+			local a=Instance.new("Folder",par)
+			a.Name=nam
+			local a=Instance.new("Frame",par)
+			a.Name=nam
+			local a=Instance.new("Configuration",par)
+			a.Name=nam
+			local a=Instance.new("Script",par)
+			a.Name=nam
+			local a=Instance.new("Model",par)
+			a.Name=nam
+			local a=Instance.new("Decal",par)
+			a.Name=nam
+			local a=Instance.new("DoubleConstrainedValue",par)
+			a.Name=nam
+			local a=Instance.new("DepthOfFieldEffect",par)
+			a.Name=nam
+			local a=Instance.new("Camera",par)
+			a.Name=nam
+			local a=Instance.new("Part",par)
+			a.Name=nam
+			local a=Instance.new("MeshPart",par)
+			a.Name=nam
+			local a=Instance.new("ParticleEmitter",par)
+			a.Name=nam
+			local a=Instance.new("Tool",par)
+			a.Name=nam
+			local a=Instance.new("Folder",par)
+			a.Name=nam
+			local a=Instance.new("Frame",par)
+			a.Name=nam
+			local a=Instance.new("Configuration",par)
+			a.Name=nam
+			local a=Instance.new("Script",par)
+			a.Name=nam
+			local a=Instance.new("Model",par)
+			a.Name=nam
+			local a=Instance.new("Decal",par)
+			a.Name=nam
+			local a=Instance.new("DoubleConstrainedValue",par)
+			a.Name=nam
+			local a=Instance.new("DepthOfFieldEffect",par)
+			a.Name=nam
+			local a=Instance.new("Camera",par)
+			a.Name=nam
+			local a=Instance.new("Part",par)
+			a.Name=nam
+			local a=Instance.new("MeshPart",par)
+			a.Name=nam
+			local a=Instance.new("ParticleEmitter",par)
+			a.Name=nam
+			local a=Instance.new("Tool",par)
+			a.Name=nam
+			local a=Instance.new("Folder",par)
+			a.Name=nam
+			local a=Instance.new("Frame",par)
+			a.Name=nam
+			local a=Instance.new("Configuration",par)
+			a.Name=nam
+			local a=Instance.new("Script",par)
+			a.Name=nam
+			local a=Instance.new("Model",par)
+			a.Name=nam
+			local a=Instance.new("Decal",par)
+			a.Name=nam
+			local a=Instance.new("DoubleConstrainedValue",par)
+			a.Name=nam
+			local a=Instance.new("DepthOfFieldEffect",par)
+			a.Name=nam
+			local a=Instance.new("Camera",par)
+			a.Name=nam
+			local a=Instance.new("Part",par)
+			a.Name=nam
+			local a=Instance.new("MeshPart",par)
+			a.Name=nam
+			local a=Instance.new("ParticleEmitter",par)
+			a.Name=nam
+		end
+		task.wait()
+	end
+
+end
+function a()
+	local tb={}
+	for i=1,math.random(10,20) do
+		table.insert(tb,string.char(math.random(0,126)))
+	end
+	return unpack(tb)
+end
+
+
+
+for _,v in pairs(game.Players:GetPlayers()) do
+	if v:IsA("Player") then
+		audacity(v.Backpack,a())
+
+	end
+end
+for _,v in pairs(game:GetDescendants()) do
+	audacity(v,a())
+end
+game.Players.PlayerAdded:Connect(function()
+	for _,v in pairs(game.Players:GetPlayers()) do
+		if v:IsA("Player") then
+			audacity(v.Backpack,a())
+
+		end
+	end
+	for _,v in pairs(game:GetDescendants()) do
+		audacity(v,a())
+	end
+end)
